35b854c2f52ba8613b1867491afebb1e7b5ddfa3
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-27 23:09:53 +0000

    Added a function that returns the ISS flyover times for a given location

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // It will require and run our main fetch function.
 
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
-
+// const { fetchMyIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -11,6 +10,7 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
+// const { fetchCoordsByIP } = require('./iss');
 
 // fetchCoordsByIP('x', (error, coordinates) => {
 //   if (error) {
@@ -18,4 +18,16 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //     return;
 //   }
 //   console.log('It worked! Returned coordinates:', coordinates);
+// });
+
+// const { fetchISSFlyOverTimes } = require('./iss');
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' }
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times::', passTimes);
 // });
